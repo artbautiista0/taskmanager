7f72dbf6c28432a51e0580778db7a6d1cb6369e4
Arturo Bautista de Jesus <abautistad@megacable.com.mx>
2025-02-10 00:00:35 -0600

    fix

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ export default {
     transform: {
       '^.+\\.js$': 'babel-jest',
     },
-    transformIgnorePatterns: ['/node_modules/(?!<paquete-que-necesites-transformar>)'],
     testEnvironment: 'jest-environment-jsdom',
   };
   
